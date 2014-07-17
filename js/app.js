9a1f9c045049e6a2119e617304091018d713e1a8
andraroston <andraroston@Andras-MacBook-Air.local>
2014-07-17 21:17:08 +0100

    Part 4A
    Mouseup and mousedown features implemented.

--- js/app.js
+++ js/app.js
@@ -2,9 +2,20 @@ $(document).ready(function() {
 	$(".ryu").mouseenter(function(){
 		$(".ryu-still").hide();
 		$(".ryu-ready").show();
-	});
-	$(".ryu").mouseleave(function(){
+	})
+	.mouseleave(function(){
 		$(".ryu-ready").hide();
 		$(".ryu-still").show();
+	})
+	.mousedown(function() {
+			// play hadouken sound
+			$(".ryu-ready").hide();
+			$(".ryu-throwing").show();
+			$(".hadouken").show();
+			// show hadouken and animate it to the right of the screen
+	})
+	.mouseup(function() {
+			$(".ryu-throwing").hide();
+			$(".ryu-ready").show();
 	});
-})
+});
